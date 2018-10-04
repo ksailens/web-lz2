c34ff96dc4d44e8f530a90de8f5640a43be55aed
ksailens <43430946+ksailens@users.noreply.github.com>
2018-10-04 16:25:56 +0300

    valid FIO field

--- script/check.js
+++ script/check.js
@@ -1,10 +1,11 @@
 function checkData() {
     var valid = true;
     var elems = document.contactsForm;
-    var phone = elems.tel.pattern;
     elems.tel.pattern = "(\\+7|\\+3)[0-9]{9,11}";
+    var textArea = elems.FIO.value;
+    var wordCounter = textArea.split(' ').length;
 
-    if (elems.question.value === '') {
+    if ((elems.question.value === '')) {
         alert ( "Пожалуйста, напишите свой вопрос." );
         elems.question.focus();
         valid = false;
@@ -27,17 +28,25 @@ function checkData() {
     }
 
 
-    if ((elems.tel.value === '') || (elems.tel.value !== phone) ) {
+    if ((elems.tel.value === '')) {
         alert ( "Пожалуйста, заполните номер телефона в формате: +7 или +3, без пробелов, от 9 до 11 цифр, включая код." );
         elems.tel.focus();
         valid = false;
     }
 
-    if (elems.FIO.value === '') {
-        alert ( "Пожалуйста, заполните поле 'Ваше ФИО'." );
+    if ((wordCounter<3) || elems.FIO.value === '') {
+        alert ( "Пожалуйста, заполните поле 'Ваше ФИО' полностью.");
         elems.FIO.focus();
         valid = false;
     }
 
     return valid;
 }
+
+
+/* function checkWord() {
+     var textArea = elems.question.value;
+     var wordCounter = textArea.split(' ');
+     alert(wordCounter);
+ }
+*/
